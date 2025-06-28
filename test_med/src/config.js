11d60ea85fe68c5f5406d8ad8c0ed76b39f4f540
Mario Benov <mario.ben@mail.bg>
2025-06-28 16:06:03 +0300

    feat: deployment

--- test_med/src/config.js
+++ test_med/src/config.js
@@ -1,4 +1,4 @@
-export const API_URL = window.location.hostname === "localhost" ? "https://mariobenov70-8181.theiadockernext-1-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai" : "https://mariobenov70-8181.theiadockernext-1-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai";
+export const API_URL = window.location.hostname === "localhost" ? "<add your server-side url>" : "<add your server-side url>";
 console.log(
     "API_URL :",
     API_URL
